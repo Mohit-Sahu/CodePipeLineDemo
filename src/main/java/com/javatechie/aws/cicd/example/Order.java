94ed87b5eddc7ba9b16f06fd625a1ccbb6227d90
MOHIT SAHU <81066198+Mohit-Sahu@users.noreply.github.com>
2024-01-08 00:21:31 +0530

    Update Order.java

--- src/main/java/com/javatechie/aws/cicd/example/Order.java
+++ src/main/java/com/javatechie/aws/cicd/example/Order.java
@@ -1,12 +1,10 @@
 package com.javatechie.aws.cicd.example;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 @ToString
 public class Order {
